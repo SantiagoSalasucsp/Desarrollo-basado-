0b5fd51b3ce7aa2452da4d5c9d353efb030d0752
Santiago <santiago.salas.sotillo@ucsp.edu.pe>
2023-11-27 21:58:07 -0500

    tarea

---
+++ tarea segunda parte/2/Formulario_CV_Xamarin-main/App3/MainPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace App3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            paisPicker.ItemsSource = new List<string>
+            {
+                "Argentina",
+                "Bolivia",
+                "Brasil",
+                "Chile",
+                "Colombia",
+                "Ecuador",
+                "Paraguay",
+                "Perú",
+                "Venezuela",
+                "Uruguay",
+            };
+        }
+
+        void OnEnviarClicked(object sender, EventArgs args)
+        {
+            // Datos del formulario
+            string nombres = nombresEntry.Text;
+            string apellidos = apellidosEntry.Text;
+            DateTime fechaDeNacimiento = fechaNacimientoPicker.Date;
+            string ocupacion = ocupacionEntry.Text;
+            string telefono = telefonoEntry.Text;
+            string email = emailEntry.Text;
+            string pais = (string)paisPicker.SelectedItem;
+
+            string nivelDeIngles = string.Empty;
+            if (inglesBasicoRadio.IsChecked)
+                nivelDeIngles = "Básico";
+            else if (inglesIntermedioRadio.IsChecked)
+                nivelDeIngles = "Intermedio";
+            else if (inglesAvanzadoRadio.IsChecked)
+                nivelDeIngles = "Avanzado";
+            else if (inglesFluidoRadio.IsChecked)
+                nivelDeIngles = "Fluido";
+
+            List<string> lenguajesProgramacion = new List<string>();
+            if (pythonCheckbox.IsChecked)
+                lenguajesProgramacion.Add("Python");
+            if (cPlusPlusCheckbox.IsChecked)
+                lenguajesProgramacion.Add("C++");
+            if (javaCheckbox.IsChecked)
+                lenguajesProgramacion.Add("Java");
+            if (rubyCheckbox.IsChecked)
+                lenguajesProgramacion.Add("Ruby");
+
+            string aptitudes = aptitudesEntry.Text;
+
+            List<string> habilidades = new List<string>();
+            if (htmlCheckbox.IsChecked)
+                habilidades.Add("Dominio de HTML");
+            if (backendCheckbox.IsChecked)
+                habilidades.Add("Desarrollo de aplicaciones Backend");
+            if (testingCheckbox.IsChecked)
+                habilidades.Add("Habilidades para testing y debugging");
+            if (responsivoCheckbox.IsChecked)
+                habilidades.Add("Diseño responsivo");
+            string perfil = perfilEditor.Text;
+
+            var resultPage = new ResultPage(nombres, apellidos, fechaDeNacimiento, ocupacion, telefono, email, pais, nivelDeIngles, lenguajesProgramacion, aptitudes, habilidades, perfil);
+
+            Navigation.PushAsync(resultPage);
+
+        }
+    }
+}
+ 
+
---
+++ tarea segunda parte/2/Formulario_CV_Xamarin-main/App3/ResultPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace App3
+{
+    public partial class ResultPage : ContentPage
+    {
+        public ResultPage(
+            string nombres,
+            string apellidos,
+            DateTime fechaDeNacimiento,
+            string ocupacion,
+            string telefono,
+            string email,
+            string pais,
+            string nivelDeIngles,
+            List<string> lenguajesProgramacion,
+            string aptitudes,
+            List<string> habilidades,
+            string perfil
+            )
+        {
+            InitializeComponent();
+
+            // Crea y agrega un Label para cada dato del formulario
+            resultStackLayout.Children.Add(new Label { Text = "Nombres: " + nombres });
+            resultStackLayout.Children.Add(new Label { Text = "Apellidos: " + apellidos });
+            resultStackLayout.Children.Add(new Label { Text = "Fecha de nacimiento: " + fechaDeNacimiento.ToString("dd/MM/yyyy") });
+            resultStackLayout.Children.Add(new Label { Text = "Ocupación: " + ocupacion });
+            resultStackLayout.Children.Add(new Label { Text = "Teléfono: " + telefono });
+            resultStackLayout.Children.Add(new Label { Text = "Email: " + email });
+            resultStackLayout.Children.Add(new Label { Text = "País: " + pais });
+            resultStackLayout.Children.Add(new Label { Text = "Nivel de Inglés: " + nivelDeIngles });
+
+            // Concatena los lenguajes de programación en una cadena
+            string lenguajes = string.Join(", ", lenguajesProgramacion);
+            resultStackLayout.Children.Add(new Label { Text = "Lenguajes de programación: " + lenguajes });
+
+            resultStackLayout.Children.Add(new Label { Text = "Aptitudes: " + aptitudes });
+
+            // Concatena las habilidades en una cadena
+            string habilidadesConcatenadas = string.Join(", ", habilidades);
+            resultStackLayout.Children.Add(new Label { Text = "Habilidades: " + habilidadesConcatenadas });
+
+            resultStackLayout.Children.Add(new Label { Text = "Perfil: " + perfil });
+        }
+    }
+}
